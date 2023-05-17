12ba075890c1e3f21c81134da46658e8a153df9f
Kai Aliani <kaialiani01@gmail.com>
2023-05-17 13:22:06 +0200

    Footer + completed LandingPage

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Navbar } from "./components/Navbar";
 import { HeroSection } from "./components/HeroSection";
+import myImage from "pages/pics/demo.jpg";
+import { Newsletter } from "./components/Newsletter";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
@@ -19,7 +22,7 @@ export default function Home() {
       </header>
       <main className={styles.Main}>
         <HeroSection />
-        <div className={styles.announcementRightPic}>
+        <div className={styles.announcement}>
           <div>
             <h2>THE BEATLES ARE COMING</h2>
             <p>
@@ -32,10 +35,33 @@ export default function Home() {
               aliqua. Aute laborum esse aute ea amet.
             </p>
           </div>
-          <div></div>
+          <div>
+            <img src={myImage} alt="My Image" />
+          </div>
         </div>
+
+        <div className={styles.announcement}>
+          <div>
+            <img src={myImage} alt="My Image" />
+          </div>
+          <div>
+            <h2>THE BEATLES ARE COMING</h2>
+            <p>
+              Officia eiusmod labore fugiat anim duis et anim consectetur enim
+              ex aute nisi aute sunt proident. Excepteur magna ea ea ut cillum
+              incididunt exercitation consequat. Dolor minim id ea ullamco
+              excepteur commodo ea labore deserunt aliquip voluptate anim aute
+              proident. Officia ut et enim Lorem veniam. Veniam sunt Lorem
+              labore Lorem aliqua velit aliquip voluptate cupidatat dolor
+              aliqua. Aute laborum esse aute ea amet.
+            </p>
+          </div>
+        </div>
+        <Newsletter />
       </main>
-      <footer></footer>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
