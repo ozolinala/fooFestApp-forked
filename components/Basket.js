c891851244db31831f75620fe85e55d5291bab4e
Laura Ozolina <laur755v@stud.kea.dk>
2023-05-18 16:24:16 +0200

    can add things to the basket

--- components/Basket.js
+++ components/Basket.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { useContext } from 'react';
+import { StoreContext } from '@/contexts/storeContext';
 
 function Basket() {
+  const state = useContext(StoreContext);
+  console.log(state);
+  console.log(typeof(state));
+  // if (!state || !state.basket || state.basket.length === 0) {
+  //   return <div>Empty basket</div>;
+  // } else {
+    function removeOne(){
+      
+    }
+
   return (
     <div>
         <h2>Basket</h2>
+        <ul>
+        {state.data.basket.map(item => {
+          return(
+            <li key={item.id} >{item.name} X {item.amount}
+       <button onClick={removeOne}>-</button></li>
+            ) 
+       })}
+        
+        </ul>
     </div>
-  )
+  );
 }
 
-export default Basket
+export default Basket;
--- contexts/storeContext.js
+++ contexts/storeContext.js
@@ -4,20 +4,40 @@ export const StoreContext = createContext();
 export const DispatchContext = createContext();
 const initialState = {
     basket:[],
-    products:[],
 };
 
 function reducer(state, action) {
-    switch (action.type){
-        case "LOGIN":
-            return {
-              ...state, name: action.payload,
-            };
-            case "LOGOUT":
-                return {
-                  ...state, name: ""
-                };
-                
+    switch (action.action){
+case "REMOVE_ONE_PRODUCT":
+
+console.log(action);
+return {
+    ...state
+};
+
+        case "ADD_PRODUCT":
+        console.log(state, action);
+        // if it exists already, add 1
+        const exists = state.basket.find((item)=>item.id === action.payload.id)
+        if(exists){
+
+const nextBasket = state.basket.map((item)=>{
+        if(item.id === action.payload.id){
+            // found it
+            const copy ={...item};
+            copy.amount++;
+            return copy;
+        } else {
+            return item;
+        }
+    });
+    return {...state, basket: nextBasket}
+        } else {
+            const newItem = action.payload;
+            newItem.amount = 1;
+            return {...state, basket: state.basket.concat(newItem)}  
+
+        }
     }
 }
 
