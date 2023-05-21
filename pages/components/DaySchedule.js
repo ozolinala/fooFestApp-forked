787d4345f52881f5654afd4ab3ac7bf8d5521cd8
Kai Aliani <kaialiani01@gmail.com>
2023-05-21 19:20:48 +0200

    capitalized and filtered data

--- pages/components/DaySchedule.js
+++ pages/components/DaySchedule.js
@@ -1,28 +1,37 @@
 import { List } from "antd";
+import styles from "@/styles/Program.module.css";
 
 function DaySchedule({ day, venues }) {
   const schedule = venues.map((venue) => ({
     venue: venue.venue,
-    events: venue.schedule[day] || [], // Set a default empty array if venue.schedule[day] is undefined
+    events: venue.schedule[day] || [],
+  }));
+
+  const filteredSchedule = schedule.map((item) => ({
+    ...item,
+    events: item.events.filter((event) => event.act !== "break"),
   }));
 
   return (
     <List
-      dataSource={schedule}
+      dataSource={filteredSchedule}
       renderItem={(item) => (
         <List.Item>
           <h2>{item.venue}</h2>
-          {item.events.length > 0 ? (
-            item.events.map((event, index) => (
-              <div key={index}>
-                <p>Act: {event.act}</p>
-                <p>Start: {event.start}</p>
-                <p>End: {event.end}</p>
-              </div>
-            ))
-          ) : (
-            <p>No events</p>
-          )}
+          <div className={styles.ActInfoContainer}>
+            {item.events.length > 0 ? (
+              item.events.map((event, index) => (
+                <div className={styles.ActInfo} key={index}>
+                  <p>{event.act.toUpperCase()}</p>
+                  <p>
+                    {event.start.split(":")[0]}-{event.end.split(":")[0]}
+                  </p>
+                </div>
+              ))
+            ) : (
+              <p>No events</p>
+            )}
+          </div>
         </List.Item>
       )}
     />
