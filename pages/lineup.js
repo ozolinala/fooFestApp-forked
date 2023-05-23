d621847f83ff0212d28e701e28ad44442a71cae5
Kai Aliani <kaialiani01@gmail.com>
2023-05-23 16:03:16 +0200

    first commit

--- pages/lineup.js
+++ pages/lineup.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Ticket from '../components/Ticket';
 import styles from "@/styles/Lineup.module.css";
+import image1 from '../public/pics';
+import image2 from '../images/image2.jpg';
+import image3 from '../images/image3.jpg';
 
 export default function Home({data}) {
   return (
