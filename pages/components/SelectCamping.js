13c932814950f94d0546db76bfe0e44f6c9b8776
Kai Aliani <kaialiani01@gmail.com>
2023-05-26 09:22:10 +0200

    better layout

--- pages/components/SelectCamping.js
+++ pages/components/SelectCamping.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "@/styles/Checkout.module.css";
 import CampingType from "./CampingType";
+import Basket from "@/components/Basket";
 
 function SelectCamping() {
   return (
     <div className={styles.CampingFlex}>
-      <div>
-        <CampingType />
-      </div>
-      <div>BASKET</div>
+      <CampingType />
+
+      <Basket />
     </div>
   );
 }
