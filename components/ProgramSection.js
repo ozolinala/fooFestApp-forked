b073e22cf033355b05af4ef0d75de642266a106b
Kai Aliani <kaialiani01@gmail.com>
2023-06-01 14:29:15 +0200

    program page fixed

--- components/ProgramSection.js
+++ components/ProgramSection.js
@@ -4,9 +4,19 @@ import styles from "@/styles/Program.module.css";
 export const ProgramSection = () => {
   return (
     <div className={styles.ProgramSection}>
-      <img className={styles.programImage} src="pics/map.webp" alt={`artist1`} />
-
-  
+      <img className={styles.programImage} src="pics/map.png" alt={`map`} />
+      <div className={styles.midgardPin}>
+        <p>Midgard</p>
+        <img src="pics/pin.svg" alt={`map`} />
+      </div>
+      <div className={styles.JotunheimPin}>
+        <p>Jotunheim</p>
+        <img src="pics/pin.svg" alt={`map`} />
+      </div>
+      <div className={styles.VanaheimPin}>
+        <p>Vanaheim</p>
+        <img src="pics/pin.svg" alt={`map`} />
+      </div>
     </div>
   );
 };
